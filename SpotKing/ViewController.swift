775ac44bdb350bf32ceb5df7865b3c88c2421877
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-05-31 13:18:40 -0400

    Initial Commit

---
+++ SpotKing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotKing
+//
+//  Created by Alejandro Zielinsky on 2018-05-31.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
