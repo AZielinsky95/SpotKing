6595ef56147298585a38bbef437937b4ee60611d
Michael Chebotarov <mchebotarov@rogers.com>
2018-06-01 16:08:42 -0400

    commit

--- SpotKing/Controller/AddSpotViewController.swift
+++ SpotKing/Controller/AddSpotViewController.swift
@@ -8,28 +8,89 @@
 
 import UIKit
 
-class AddSpotViewController: UIViewController {
+class AddSpotViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var spotTitle: UITextField!
+    @IBOutlet weak var spotDescription: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePickedBlock: ((UIImage) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(AddSpotViewController.imageSelectorTapped(recognizer:)))
+        tapGesture.delegate = self
+        imageView.addGestureRecognizer(tapGesture)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func imageSelectorTapped(recognizer:UITapGestureRecognizer) {
+        showActionSheet(vc: self)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func postSpot() {
+        
     }
-    */
-
+    
+    func camera()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func photoLibrary()
+    {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .photoLibrary
+            self.present(myPickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func showActionSheet(vc: UIViewController) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+            self.camera()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+            self.photoLibrary()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        vc.present(actionSheet, animated: true, completion: nil)
+    }
 }
+
+extension AddSpotViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+         //   self.imagePickedBlock?(image)
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }else{
+            print("Something went wrong")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
