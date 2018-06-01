7ba74d1b38479142e18b0a1f644d39ec7ffeb174
Michael Chebotarov <mchebotarov@rogers.com>
2018-06-01 13:54:46 -0400

    commit save/get spot in DBManager

--- SpotKing/Managers/DatabaseManager.swift
+++ SpotKing/Managers/DatabaseManager.swift
@@ -8,31 +8,71 @@
 
 import Foundation
 import FirebaseDatabase
+import FirebaseDatabase
+import CoreLocation
 
 class DatabaseManager
 {
-    static var ref : DatabaseReference = {
+    lazy var ref : DatabaseReference = {
         return Database.database().reference()
     }()
     
-    static func saveSkateSpot(spot:SkateSpot)
+    func saveSkateSpot(spot:SkateSpot)
     {
         let generatedRef = ref.child("skatespots").childByAutoId()
         
         generatedRef.observe(.value, with: { (snapshot) in
-
+            
             let newSpot = [ // 2
-                "userID": spot.userID,
-                "rating": spot.spotRating,
-                "spotType": spot.spotType.hashValue,
+                "userID": snapshot.childrenCount,
+                "rating": spot.spotRating ?? 0,
+                "spotType": spot.spotType.toString(),
                 "lat": spot.coordinate.latitude,
                 "lng": spot.coordinate.longitude,
                 "title": spot.title!,
                 "subTitle": spot.subtitle!,
-
+                
                 ] as [String : Any]
             
             generatedRef.setValue(newSpot);
-        })        
+            
+        })
+        
+        
+        
+    }
+    
+    func getSkateSpots(completion: @escaping ([SkateSpot])->()) {
+        var skateSpots = [SkateSpot]()
+        
+        let spotRef = ref.child("skatespots")
+        spotRef.observe(.value, with: { (snapshot) in
+            
+            for spot in snapshot.children {
+                
+                guard let spotSnapshot = spot as? DataSnapshot, let value = spotSnapshot.value as? NSDictionary else {
+                    continue
+                }
+                
+                let title = value["title"] as? String ?? ""
+                let subtitle = value["subTitle"] as? String ?? ""
+                let spotTypeStr = value["spotType"] as? String ?? ""
+                let spotType = SkateSpot.SpotType.toSpotType(strSpotType: spotTypeStr)
+                let coordinate = CLLocationCoordinate2DMake((value["lat"] as? Double)!, (value["lng"] as? Double)!)
+                let spotRating = value["rating"] as? Double
+                let userID = value["userID"] as? Int
+                
+                
+                let skateSpot = SkateSpot(userId: userID!, type: spotType, title: title, subtitle: subtitle, rating: spotRating, pinImage: nil, coordinates: coordinate)
+
+                
+                skateSpots.append(skateSpot)
+                
+            }
+            completion(skateSpots)
+            
+        })
+        
+        
     }
 }
--- SpotKing/Model/SkateSpot.swift
+++ SpotKing/Model/SkateSpot.swift
@@ -16,7 +16,33 @@ class SkateSpot : NSObject, MKAnnotation
         case SkatePark
         case SkateShop
         case SkateSpot
+        
+        func toString() -> String {
+            switch self {
+            case .SkatePark:
+                return "SkatePark"
+            case .SkateShop:
+                return "SkateShop"
+            case .SkateSpot:
+                return "SkateSpot"
+            }
+        }
+        
+        static func toSpotType(strSpotType: String) -> SpotType {
+            switch strSpotType {
+            case "SkatePark":
+                return SpotType.SkatePark
+            case "SkateShop":
+                return SpotType.SkateShop
+            case "SkateSpot":
+                return SpotType.SkateSpot
+            default:
+                return SpotType.SkatePark
+            }
+        }
     }
+    
+
     
     var userID: Int!
     var spotType:SpotType!
